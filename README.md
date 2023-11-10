8f7519548f50468df2e90b2c0d832991826ca1e3
ART_List <artbdarm9@mail.ru>
2023-11-10 21:56:10 +0300

    feat: implement geolocation and user permission

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Weather app on Flutter
 Weather mobile application based on the beautiful design of [Olia Gozha](https://dribbble.com/shots/4663154-).
 
 ## Packages used:
+1. [geolocator](https://pub.dev/packages/geolocator/install)
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,4 @@
+import 'package:sunnyside/services/location.dart';
 import 'package:flutter/material.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -7,13 +8,25 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
+        child: ElevatedButton(
+          onPressed: () {},
           child: const Text('Get Location'),
         ),
       ),
